e2e869ab6c334d292859e22b7f01354f8b1972ec
Mark Smith <aardmark.smith@gmail.com>
2025-05-08 23:07:16 +1000

    P: Revert "K: scan cc,ssn and fone"
    This reverts commit a33c75d4194036a5cc234ccaef2c35b5ffd61f84.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,13 +1,1 @@
-echo "\n===== Scanning for credit card numbers ======"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "============== CREDIT CARD SCAN COMPLETE ==============="
-
-echo "\n===== Scanning for social security numbers ======"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "============== SOCIAL SECURITY SCAN COMPLETE ==============="
-
-echo "\n===== Scanning for phone numbers ======"
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "============== PHONE NUMBER SCAN COMPLETE ==============="
-
+# TODO: write the script
